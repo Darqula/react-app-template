df456d453ff54321d6e3a8fe2f32ff7a65ed1e29
Vlad_Raven <vladiksat@list.ru>
2024-12-14 17:22:02 +0700

    Extracted App component from index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 
 document.body.innerHTML = '<div id="root"></div>'
 
 const app = createRoot(document.getElementById('root')!)
-app.render(
-  <div>
-    Hello world!
-  </div>
-)
+app.render(<App />)
